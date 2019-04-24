df00cfe34ac7c948fcbca53e597d9e32503f2117
puterakahfi <puterakahfi@gmail.com>
2019-04-24 22:54:17 +0700

    update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,18 +20,7 @@
 
     <footer class="footer">
       <slot name="footer">
-        <div class="content has-text-centered ">
-          <p>
-            <strong>Bulma</strong> by
-            <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
-            licensed
-            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-            The website content is licensed
-            <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
-              >CC BY NC SA 4.0</a
-            >.
-          </p>
-        </div>
+        <Footer></Footer>
       </slot>
     </footer>
   </div>
@@ -39,10 +28,12 @@
 
 <script>
 import Navbar from '~/components/Navbar.vue'
+import Footer from '~/components/Footer.vue'
 import Hero from '~/components/Hero.vue'
 export default {
   components: {
     Navbar,
+    Footer,
     Hero
   }
 }
