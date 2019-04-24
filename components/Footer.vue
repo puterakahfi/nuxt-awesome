36f9661ea251fbe659025f2164f49ec1a2cfd094
puterakahfi <puterakahfi@gmail.com>
2019-04-24 22:51:45 +0700

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="content has-text-centered">
+    <a href="https://bulma.io">
+      <img
+        src="https://bulma.io/images/made-with-bulma--dark.png"
+        alt="Made with Bulma"
+        width="128"
+        height="24"
+      />
+    </a>
+
+    <ul class="is-marginless has-margin-top-5">
+      <li>
+        <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+      </li>
+      <li>
+        <a href="https://vuejs.org" target="_blank">vuejs.org</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+li {
+  font-size: 1em;
+  display: inline;
+  a {
+    color: #707070;
+  }
+}
+</style>
